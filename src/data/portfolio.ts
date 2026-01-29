c30afc2ccd32545c9e90788c95c8897912f10d6e
Max Andersson <max.david.albin.andersson@cern.ch>
2026-01-29 20:31:16 +0100

    change to relative paths for data

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -4,28 +4,28 @@ export const portfolioLocations: PortfolioLocation[] = [
   {
     id: "paris",
     location: "Paris",
-    coverImage: "/portfolio/paris/DSCF5564.JPG",
+    coverImage: "photo_portfolio/paris/DSCF5564.JPG",
     photos: [
-      "/portfolio/paris/DSCF5564.JPG",
+      "photo_portfolio/paris/DSCF5564.JPG",
     ],
   },
   {
     id: "florence",
     location: "Florence",
-    coverImage: "/portfolio/florence/DSCF1759.JPG",
+    coverImage: "photo_portfolio/florence/DSCF1759.JPG",
     photos: [
-        "/portfolio/florence/DSCF1759.JPG",
-        "/portfolio/florence/DSCF1749.JPG",
-        "/portfolio/florence/DSCF1598.JPG",
-        "/portfolio/florence/DSCF1705.JPG",
+        "photo_portfolio/florence/DSCF1759.JPG",
+        "photo_portfolio/florence/DSCF1749.JPG",
+        "photo_portfolio/florence/DSCF1598.JPG",
+        "photo_portfolio/florence/DSCF1705.JPG",
     ],
   },
   {
     id: "lauterbrunnen",
     location: "Lauterbrunnen",
-    coverImage: "/portfolio/lauterbrunnen/DSCF7150.JPG",
+    coverImage: "photo_portfolio/lauterbrunnen/DSCF7150.JPG",
     photos: [
-        "/portfolio/lauterbrunnen/DSCF7150.JPG",
+        "photo_portfolio/lauterbrunnen/DSCF7150.JPG",
         // "/portfolio/lauterbrunnen/DSCF1749.JPG"
     ],
   },
